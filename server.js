283b27360b5af8e9992585a19e3165a6cd46254b
cpaschall <cpaschall216@gmail.com>
2022-06-13 15:17:45 -0400

    updating file paths

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/blogRoutes'));
+app.use(require('./controllers/api/blogRoutes'));
 
 // app.get('/', (req, res) => (
 //     res.send("Hello World!")
