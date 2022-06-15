6dfcc52aac2facf245accb77b65186f11c50dd94
cpaschall <cpaschall216@gmail.com>
2022-06-14 22:16:25 -0400

    updating route files

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+const { route } = require('../homeRoutes');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.isSoftDeleted;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+route.post('/login', async (req, res) => {
+
+    try {
+        const userData = await User.findOne({ where: { user_name: req.body.user_name } });
+
+        if (!userdata) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.isSoftDeleted;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,80 @@
+const router = require('express').Router();
+const { User, Article } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const articleData = await Article.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['user_name']
+                },
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const articles = articleData.map((article) = article.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            articles,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('article/:id', async (req, res) => {
+    try {
+        const articleData = await Article.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['user_name'],
+                },
+            ],
+        });
+
+        const article = articleData.get({ plain: true });
+
+        res.render('article', {
+            // '...' is the spread syntax which indicates an indivudal item within a array
+            ...article,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Article },]
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('article', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
