39411c15f771adda1e54028e0a9b8c8ea76d02b4
cpaschall <cpaschall216@gmail.com>
2022-06-18 20:43:18 -0400

    refactoring user routes and login logic

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,62 +1,98 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const User = require('../../models/User');
+const withAuth = require('../../utils/auth')
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
-        const userData = await User.create(req.body);
-
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-
-            res.status(200).json(userData);
-        });
+      const userData = await User.create({
+                user_name: req.body.user_name,
+                password: req.body.password
+              });
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
     } catch (err) {
-        res.status(400).json(err);
+      res.status(400).json(err);
     }
-});
+  });
 
-router.post('/login', async (req, res) => {
-
+  router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { user_name: req.body.user_name } });
+      const userData = await User.findOne({ where: { user_name: req.body.user_name } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user_name: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
-        if (!userdata) {
-            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: {
+                id: req.params.id,
+                // user_id: req.session.user_id,
+            },
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!' });
             return;
         }
 
-        const validPassword = await userData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect username or password, please try again' });
-            return;
-        }
-
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-
-            res.json({ user: userData, message: 'You are now logged in!' });
-        });
+        res.status(200).json(userData);
     } catch (err) {
-        res.status(400).json(err);
+        res.status(500).json(err);
     }
 });
 
-router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
+router.get("/", async (req, res) => {
+    // find all users
+    try {
+      const userData = await User.findAll({
+        attributes: ["id", "user_name","password"],
+      });
+      res.json(userData);
+    } catch (error) {
+      res.status(400).json({ message: "Server is down" });
     }
-});
+  });
 
-router.get('/seeUsers', async (req, res) => {
-    userData = await User.findAll();
-    res.status(200).json(userData);
-});
+  router.post('/logout', (res,req) =>{
+    if(req.session.logged_in) {
+      req.session.destroy(() =>{
+        res.status(204).end();
+      })
+    }else {
+      res.status(404).end();
+    }
+  })
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,9 @@
-// // Dependencies
-// const express = require('express');
-// const exphbs = require('express-handlebars');
-// const path = require('path');
-// const hbs = exphbs.create({});
-// const sequelize = require('./config/connection');
-
-// // Sets up the Express App
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-// // Sets up the routes
-// app.use(require('./controllers/'));
-
-// // Starts the server to begin listening
-// sequelize.sync({ force: true }).then(() => {
-//     app.listen(PORT, () => console.log('Server listening on PORT:' + PORT));
-// });
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const hbs = exphbs.create({});
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -38,7 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
+const FORCETABLE = process.env.FORCETABLE;
 
 const sess = {
   secret: 'Super secret secret',
@@ -62,6 +37,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+sequelize.sync({ force: FORCETABLE }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
